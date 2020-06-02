29b62d1e90641815e9a351334371b951b4928000
Nathalia Cristina Gomes Oliveira <nathaliacristina20@hotmail.com>
2020-06-02 18:57:18 -0300

    feat(backend): add config knex, migrations, seeds

---
+++ server/src/routes.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+import knex from './database/connection';
+
+const routes = express.Router();
+
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item => {
+    return {
+      name: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`
+    };
+  });
+
+  return response.json(serializedItems)
+})
+
+export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,14 @@
 import express from 'express';
+import routes from './routes';
+
+import path from 'path';
 
 const app =  express();
+
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(process.env.PORT || 3333, () => {
   console.log("Servidor executando na porta 3333! 🚀");
