46c703fc4422946e664d4e21e1374e0122530692
Nathalia Cristina Gomes Oliveira <nathaliacristina20@hotmail.com>
2020-06-03 19:53:20 -0300

    chore(backend): add eslint, prettier and cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,18 @@
 import express from 'express';
-import routes from './routes';
+
+import cors from 'cors';
 
 import path from 'path';
+import routes from './routes';
 
-const app =  express();
+const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use('/uploads', express.static(path.resolve(__dirname, "..", "uploads")));
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
-app.listen(process.env.PORT || 3333, () => {
-  console.log("Servidor executando na porta 3333! 🚀");
-})
+app.listen(3333, () => {
+  console.log('Servidor executando na porta 3333! 🚀');
+});
