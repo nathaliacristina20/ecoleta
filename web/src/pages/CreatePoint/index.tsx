45e9809eadad5939377c4fd1734472856ba3bb68
Nathalia Cristina Gomes Oliveira <nathaliacristina20@hotmail.com>
2020-06-05 22:44:25 -0300

    chore(web): capture initial position for show in the map

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, ChangeEvent } from "react";
 
+import { LeafletMouseEvent } from 'leaflet';
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import { Map, TileLayer, Marker } from "react-leaflet";
@@ -26,11 +27,22 @@ interface IBGECityResponse {
 
 const CreatePoint = () => {
   const [items, setItems] = useState<Item[]>([]);
+
   const [ufs, setUfs] = useState<string[]>([]);
   const [cities, setCities] = useState<string[]>([]);
+
+  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
   
   const [selectedUf, setSelectedUf] = useState("0");
   const [selectedCity, setSelectedCity] = useState("0");
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude} = position.coords;
+      setInitialPosition([latitude, longitude]);
+    })
+  }, []);
 
   useEffect(() => {
     async function load() {
@@ -78,6 +90,12 @@ const CreatePoint = () => {
     setSelectedCity(city);
   }
 
+  function handleMapClick(event: LeafletMouseEvent) {
+    setSelectedPosition([
+      event.latlng.lat, event.latlng.lng
+    ])
+  }
+
   return (
     <div id="page-create-point">
       <header>
@@ -123,12 +141,12 @@ const CreatePoint = () => {
             <span>Selecione o endereço no mapa</span>
           </legend>
 
-          <Map center={[-27.2092052, -49.6401092]} zoom={15}>
+          <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
             <TileLayer
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[-27.2092052, -49.6401092]} />
+            <Marker position={selectedPosition} />
           </Map>
 
           <div className="field-group">
