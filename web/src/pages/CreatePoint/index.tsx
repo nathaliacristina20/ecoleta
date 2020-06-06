53233281e35e5ae6091c77c1f86c3c396b5a6c23
Nathalia Cristina Gomes Oliveira <nathaliacristina20@hotmail.com>
2020-06-05 21:39:21 -0300

    chore(web): add form Create Point

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,31 +1,111 @@
 import React from "react";
 
-import { FiLogIn } from 'react-icons/fi';
+import { Link } from "react-router-dom";
 
-import "./styles.css";
+import { FiArrowLeft } from "react-icons/fi";
 
 import logo from "../../assets/logo.svg";
 
+import "./styles.css";
+
 const CreatePoint = () => {
   return (
-    <div id="page-home">
-      <div className="content">
-        <header>
-          <img src={logo} alt="Ecoleta" />
-        </header>
-        <main>
-          <h1>Seu marketplace de coleta de resíduos.</h1>
-          <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt="Ecoleta" />
 
-          <a href="/cadastro">
-            <span>
-              <FiLogIn />
-            </span>
-            <strong>Cadastre um ponto de coleta</strong>
-          </a>
+        <Link to="/create-point">
+          <FiArrowLeft />
+          Voltar para home
+        </Link>
+      </header>
 
-        </main>
-      </div>
+      <form>
+        <h1>
+          Cadastro do <br /> ponto de coleta
+        </h1>
+
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+
+          <div className="field">
+            <label htmlFor="name">Nome da entidade</label>
+            <input type="text" name="name" id="name" />
+          </div>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="email">E-mail</label>
+              <input type="email" name="email" id="email" />
+            </div>
+
+            <div className="field">
+              <label htmlFor="whatsapp">Whatsapp</label>
+              <input type="text" name="whatsapp" id="whatsapp" />
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Endereço</h2>
+            <span>Selecione o endereço no mapa</span>
+          </legend>
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="uf">Estado (UF)</label>
+              <select name="uf" id="uf">
+                <option value="0">Selecione uma UF</option>
+              </select>
+            </div>
+
+            <div className="field">
+              <label htmlFor="uf">Cidade</label>
+              <select name="city" id="city">
+                <option value="0">Selecione uma cidade</option>
+              </select>
+            </div>
+
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Itens de coleta</h2>
+            <span>Selecione um ou mais itens abaixo</span>
+          </legend>
+
+          <ul className="items-grid">
+            <li>
+              <img src="" alt="Teste"/>
+              <span>Oleo de cozinha</span>
+            </li>
+            <li>
+              <img src="" alt="Teste"/>
+              <span>Oleo de cozinha</span>
+            </li>
+            <li>
+              <img src="" alt="Teste"/>
+              <span>Oleo de cozinha</span>
+            </li>
+            <li>
+              <img src="" alt="Teste"/>
+              <span>Oleo de cozinha</span>
+            </li>
+            <li>
+              <img src="" alt="Teste"/>
+              <span>Oleo de cozinha</span>
+            </li>
+            <li>
+              <img src="" alt="Teste"/>
+              <span>Oleo de cozinha</span>
+            </li>
+          </ul>
+        </fieldset>
+        <button type="submit">Cadastrar ponto de coleta</button>
+      </form>
     </div>
   );
 };
